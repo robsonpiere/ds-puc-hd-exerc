2e7f4584174097c527494af86b89da57f2f72ecb
Robson Piere <robsonpiere@outlook.com>
2017-06-19 23:33:20 -0300

    Exerc 2.1

---
+++ exerc21.py
@@ -0,0 +1,42 @@
+'''
+Aluno: Robson Henrique Piere
+robsonpiere@outlook.com
+'''
+
+import mincemeat
+import glob
+import csv
+
+text_files = glob.glob('textos\\*')
+def file_contents(file_name):
+	f = open(file_name)
+	try:
+		return f.read()
+	finally:
+		f.close()
+
+def mapfn(k,v):
+	print 'map ' + k
+	from stopwords import allStopWords
+	for line in v.splitlines():
+		for word in line.split():
+			if (word not in allStopWords):
+				yield word,1
+
+def reducefn(k,v):
+	print 'reduce ' + k
+	return sum(v)
+
+source = dict((file_name,file_contents(file_name)) for file_name in text_files)
+
+s = mincemeat.Server()
+
+s.datasource = source
+s.mapfn = mapfn
+s.reducefn = reducefn
+
+results = s.run_server(password="changeme")
+
+w = csv.writer(open("RESULT.CSV","w"))
+for k, v in results.items():
+	w.writerow([k,v])
